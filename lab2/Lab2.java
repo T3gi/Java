4ff56318dc1f26e19d1ae1d471d67553a9b56993
T3gi <tytok.volodymyr@lll.kpi.ua>
2025-12-05 10:11:48 +0200

    Counter fix

--- lab2/Lab2.java
+++ lab2/Lab2.java
@@ -10,43 +10,81 @@ public class Lab2 {
 
     public static int[] wordsCounter(StringBuilder text, StringBuilder[] words){
         int result[] = new int[words.length];
+        int semi_result[];
         int next_dot;
+        int next_exlamation;
+        int next_question;
+        int next_end;
         int next_comma;
         int next_space;
         int next_separator;
         StringBuilder word = new StringBuilder();
-        while (!text.isEmpty()){
-            next_dot = text.indexOf(".");
-            next_comma = text.indexOf(",");
-            next_space = text.indexOf(" ");
-            if (next_dot < 1){
-                next_dot = text.length() + 100;
+        StringBuilder sentence = new StringBuilder();
+        StringBuilder textInput = text;
+        while (textInput.length() != 0){
+            next_dot = textInput.indexOf(".");
+            next_exlamation = textInput.indexOf("!");
+            next_question = textInput.indexOf("?");
+            if (next_dot == -1) {
+                next_dot = textInput.length() + 100;
             }
-            if (next_comma < 1){
-                next_comma = text.length() + 100;
+            if (next_exlamation == -1) {
+                next_exlamation = textInput.length() + 100;
             }
-            if (next_space < 1){
-                next_space = text.length() + 100;
+            if (next_question == -1) {
+                next_question = textInput.length() + 100;
             }
-            next_separator = Math.min(next_dot, next_comma);
-            next_separator = Math.min(next_separator, next_space);
-            word = new StringBuilder(text.substring(0, next_separator).toLowerCase());
-            for (int i = 0; i < words.length; i++){
-                if (word.toString().equals(words[i].toString())){
+            next_end = Math.min(next_dot, next_exlamation);
+            next_end = Math.min(next_end, next_question);
+            if (next_end == textInput.length() + 100){
+                next_end = textInput.length();
+            }
+            sentence = new StringBuilder(textInput.substring(0, next_end));
+            sentence.append(" ");
+            semi_result = new int[words.length];
+            while (sentence.length() != 0){
+                next_comma = sentence.indexOf(",");
+                next_space = sentence.indexOf(" ");
+                if (next_comma < 1){
+                    next_comma = sentence.length() + 100;
+                }
+                if (next_space < 1){
+                    next_space = sentence.length() + 100;
+                }
+                next_separator = Math.min(next_comma, next_space);
+                if (next_separator == sentence.length() + 100){
+                    next_separator = sentence.length();
+                }
+                word = new StringBuilder(sentence.substring(0, next_separator).toLowerCase());
+                for (int i = 0; i < words.length; i++){
+                    if (word.toString().equals(words[i].toString())){
+                        semi_result[i]++;
+                    }
+                }
+                if (next_separator == next_comma){
+                    next_separator++;
+                }
+                sentence = new StringBuilder(sentence.substring(next_separator + 1));
+            }
+            if (next_end + 2 <= textInput.length()){
+                next_end += 2;
+            } else if (next_end + 1 <= textInput.length()){
+                next_end++;
+            }
+            textInput = new StringBuilder(textInput.substring(next_end));
+            for (int i = 0; i < result.length; i++){
+                if (semi_result[i] > 0){
                     result[i]++;
                 }
             }
-            if (next_separator == next_comma || next_separator == next_dot){
-                next_separator++;
-            }
-            text.delete(0, next_separator + 1);
         }
+
         return result;
     }
 
     public static void printCounter(StringBuilder[] words, int[] counter){
         for (int i = 0; i < words.length; i++){
-            System.out.println(words[i] + ": " + counter[i] + " times");
+            System.out.println(words[i] + ": " + counter[i] + " sentences");
         }
     }
 
